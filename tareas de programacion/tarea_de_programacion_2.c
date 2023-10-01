be3bb98c058b5e5aa5558690494f85503a271746
qjari shekka <1032234412@pfur.ru>
2023-10-01 20:05:04 +0300

    'tareas'

---
+++ tareas de programacion/tarea_de_programacion_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int area_circulo ,area_cuadrado,radio_ceirculo, lado;
+
+    printf("введите 3 числа:\n");
+
+    scanf("%d",&area_circulo);
+    scanf("%d",&area_cuadrado);
+
+// si el radio del circulo es menor o igual a la mitad del lado del cuadrado entonces el circulo entra en el cuadrado
+
+
+
+
+
+}
