56530c93c41a70de45bc7b5d0b2fced96f5e4f30
qjari shekka <1032234412@pfur.ru>
2023-10-13 00:18:22 +0300

    tarea_de_programacion

---
+++ clases/clase1.c
@@ -0,0 +1,17 @@
+#include <conio.h>
+#include <graphics.h>
+
+void Tr(int x, int y, int c)
+{
+
+}
+
+main ()
+{
+
+    initwindow(400,300);
+    Tr(100,100,COLOR(0,0,255));
+    Tr(200,100,COLOR(0,0,255));
+    Tr(200,160,COLOR(0,0,255));
+    closegraph();
+}
