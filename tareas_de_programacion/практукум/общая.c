fba56ae4a1975717384f9b42565a3fd9ca143b9d
qjarishekka <1032234412@pfur.ru>
2023-11-14 14:24:14 +0300

    tarea de programacion

---
+++ tareas_de_programacion/практукум/общая.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <time.h>
+#define lim1 10
+#define lim2 10
+
+int main() {
+
+int arreglo[lim1][lim2]
+int random = 0;
+int i = 0;
+int j = 0;
+
+for(i=0;i<lim1;i++){
+
+    for(i=0;i<lim2;i++){
+
+        srand(time(NULL));
+        numero = rand() % 10
+
+    }
+
+
+}
+
+
+
+
+}
