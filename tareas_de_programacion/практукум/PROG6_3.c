fc5fd9fd4d8512bd25b96d61ba505ca2d723c843
qjari shekka <1032234412@pfur.ru>
2023-11-16 01:08:43 +0300

    tarea de programacion completada

--- tareas_de_programacion/практукум/PROG6_3.c
+++ tareas_de_programacion/практукум/PROG6_3.c
@@ -2,36 +2,33 @@
 #include<stdlib.h>
 #include <time.h>
 #define lim1 10
-#define lim2 10
 #define limrand 11
 
 int main() {
 
-int m = 0; 
 int n = 0;
-int arreglo[lim1][lim2];
+int arreglo[lim1][lim1];
 int random = 0;
 int i = 0;
 int j = 0;
 int k = 0;
 int suma = 0;
-int min = 0;
+int min = limrand;
 
 do{
 
-printf("введите значение для \"n\" и \"m\" не больше десяти и не меньше нуля\n");
+printf("введите значение для \"n\" не больше десяти и не меньше нуля\n");
 
 scanf("%d",&n);
-scanf("%d",&m);
 
-}while(n<=0 || n >lim1 || m<=0 || m>lim2);
+}while(n<=0 || n >lim1);
 
 srand(time(NULL));
 
 for(i=0;i<n;i++){
 
-    for(j=0;j<m;j++){
-
+    for(j=0;j<n;j++){
+ 
         arreglo[i][j] = rand() % limrand;
 
     }
@@ -41,7 +38,7 @@ for(i=0;i<n;i++){
 
 for(i=0;i<n;i++){
     
-    for(j=0;j<m;j++){
+    for(j=0;j<n;j++){
        
        printf("%d\t",arreglo[i][j]);
     
@@ -53,22 +50,41 @@ for(i=0;i<n;i++){
 
 for(i=0;i<n;i++){
 
-    for(k=n;k>=0;k--){
+    for(j=0;j<n;j++){
 
-        for(j=0;j<m;j++){
+        if(j<n-i-1){
 
             suma = suma + arreglo[i][j];
 
         }
+       
+    }
 
+}
 
+for(i=n-1;i>-1;i--){
+
+    for(j=n-1;j>-1;j--){
+
+        if(j>n-i-1){
+
+            if(arreglo[i][j] <= min){
+
+                min = arreglo[i][j];
+
+            }
+        }
 
     }
 
+}
 
-} 
 
-printf("сумма элементов, расположенных выше побочной диагонали матрицы равна: %d\n",suma);
+
+
+
+printf("сумма элементов, расположенных выше побочной диагонали матрицы равна: %d\n  наименьшее значение среди элементов, расположенных ниже побочной диагонали матрицы равно: %d",suma,min);
+
 
 }
 
