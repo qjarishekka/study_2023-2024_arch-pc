b2e344958e90b62b987ae1043b89a74e07d37144
qjarishekka <1032234412@pfur.ru>
2023-11-15 15:24:44 +0300

    tarea de programacion

---
+++ experimentos/piezas de codigo para comprobar.c
@@ -0,0 +1,4 @@
+//imprimir una matriz de n x m
+
+    //for(i=0;i<n;i++){for(j=0;j<m;j++){printf("%d\t",arreglo[i][j]);}printf("\n\n");}
+
--- tareas_de_programacion/практукум/общая.c
+++ tareas_de_programacion/практукум/общая.c
@@ -1,26 +1,44 @@
 #include <stdio.h>
+#include<stdlib.h>
 #include <time.h>
 #define lim1 10
 #define lim2 10
 
 int main() {
 
-int arreglo[lim1][lim2]
+int m = 0; 
+int n = 0;
+int arreglo[lim1][lim2];
 int random = 0;
 int i = 0;
 int j = 0;
 
-for(i=0;i<lim1;i++){
 
-    for(i=0;i<lim2;i++){
+do{
 
-        srand(time(NULL));
-        numero = rand() % 10
+printf("введите значение для \"n\" и \"m\" не больше десяти и не меньше нуля\n");
+
+scanf("%d",&n);
+scanf("%d",&m);
+
+}while(n<=0 || n >10 || m<=0 || m>10);
+
+
+
+for(i=0;i<n;i++){
+
+    for(j=0;j<m;j++){
+
+        arreglo[i][j] = rand() % 11;
 
     }
 
 
 }
+
+//for(i=0;i<n;i++){for(j=0;j<m;j++){printf("%d\t",arreglo[i][j]);}printf("\n\n");}
+
+
 
 
 
