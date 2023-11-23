0b7e1173ab2b3338ff219eac8e74d54720840de4
qjari shekka <1032234412@pfur.ru>
2023-11-24 01:14:58 +0300

    hola

--- tareas_de_programacion/практикум2/общая.c
+++ tareas_de_programacion/практикум2/общая.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char * PalabraMasGrande(char * CadenaDeSimbolos);
+
+
 int main(){
 
+char * CadenaDeSimbolos;
+
+
+
+printf("\nэта программа выводит самое длинное слово из введенной строки\n");
+
+printf("введите строку");
+
+scanf("%s",&CadenaDeSimbolos);
+
+printf("%s",CadenaDeSimbolos);
 
 
     
