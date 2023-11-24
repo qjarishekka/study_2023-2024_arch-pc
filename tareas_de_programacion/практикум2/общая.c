5a9c3d3d9e032b24dd54226e816464eb4b7c8207
qjarishekka <1032234412@pfur.ru>
2023-11-24 15:24:42 +0300

    actualizacion de tarea

--- tareas_de_programacion/практикум2/общая.c
+++ tareas_de_programacion/практикум2/общая.c
@@ -1,23 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-char * PalabraMasGrande(char * CadenaDeSimbolos);
+#define lim 100
+#define lim2 20
+
+char * PalabraMasGrande(char * CadenaDeSimbolos){
+
+int i = 0;
+char palabra_temp[lim2];
+int contador_de_letras = 0;
+char frase[lim];
+int inicio_de_palabra_mas_grande = 0;
+int ubicacion_de_espacio[lim];
+int j = 0;
+
+
+strcpy(frase,CadenaDeSimbolos);
+
+for(i=0;i<lim;i++){
+
+    if(frase[i] == ' ' || frase[i] == '\t' || frase[i] == '\v'){    
+    
+        ubicacion_de_espacio[j]= i;
+        j++;
+
+
+        if(i >= i - inicio_de_palabra_mas_grande){
+
+            contador_de_letras = i - inicio_de_palabra_mas_grande ;
+
+            inicio_de_palabra_mas_grande = i - contador_de_letras;
+
+        }
+
+    }
+
+   
+
+}
+
+printf("%d\n",contador_de_letras);
+
+
+return "hola";
+
+
+
+
+
+
+}
 
 
 int main(){
 
-char * CadenaDeSimbolos;
+char * cadena;
+char cadenaGuardado[lim];
+char mensaje[lim2];
 
 
 
 printf("\nэта программа выводит самое длинное слово из введенной строки\n");
+printf("введите строку\n");
 
-printf("введите строку");
+fgets(cadenaGuardado,90,stdin);
 
-scanf("%s",&CadenaDeSimbolos);
-
-printf("%s",CadenaDeSimbolos);
+cadena = cadenaGuardado;
 
 
-    
+
+
+
+printf("%s\n",PalabraMasGrande(cadena));
+
+
+
 }
+
+
+
+
+
+
+
