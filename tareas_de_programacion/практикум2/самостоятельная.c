faaf26e7a57b35a6036d90877f0022345bf26f03
qjari shekka <1032234412@pfur.ru>
2023-11-28 23:42:45 +0300

    actualizacion tarea

---
+++ tareas_de_programacion/практикум2/самостоятельная.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+#define lim 1000
+
+int NumeroDePalabrasIgualesALaPrimera(char frase[lim]){
+
+int NumeroDeLetrasDeLaPrimeraPalabra = 0;
+int NumeroDeLetras = 0;
+int i = 0;
+int NumeroDePalabrasDelMismoTamano = 0;
+int InicioDePalabra = 0;
+
+
+
+for(i=0;i<lim;i++){
+
+    if(frase[i] == ' ' || frase[i] == '\n' || frase[i] == '\v'){
+
+        i = lim;
+
+    }
+
+    NumeroDeLetrasDeLaPrimeraPalabra++;
+
+    
+
+}
+
+NumeroDeLetrasDeLaPrimeraPalabra--;
+
+
+for(i=0;i<lim;i++){
+
+    if(frase[i] == ' ' || frase[i] == '\n' || frase[i] == '\v' || frase[i] == '\0'){
+
+        if(i - InicioDePalabra == NumeroDeLetrasDeLaPrimeraPalabra ){
+
+            NumeroDePalabrasDelMismoTamano++;
+
+            
+
+        }
+
+        if(frase[i] == '\0'){
+
+            i = lim;
+
+        }
+
+        InicioDePalabra = i+1;
+
+    }
+
+}
+
+
+
+
+return NumeroDePalabrasDelMismoTamano;
+
+
+
+}
+
+
+
+
+
+int main(){
+
+char CadenaDeGuardado[lim];
+char * texto;
+
+
+printf("\nэта программа выводит число повторений первого слова\n");
+printf("введите строку\n");
+
+fgets(CadenaDeGuardado,lim,stdin);
+
+
+
+printf("число повторений первого слова: %d",NumeroDePalabrasIgualesALaPrimera(CadenaDeGuardado));
+
+}
