4e5271d558d3839864c57a59a2e6ba79c3682e90
qjarishekka <1032234412@pfur.ru>
2023-11-28 20:26:13 +0300

    tarea actualizada

--- tareas_de_programacion/практикум2/общая.c
+++ tareas_de_programacion/практикум2/общая.c
@@ -1,49 +1,39 @@
+//declaracion de librerias (объявление переменных)
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define lim 100
-#define lim2 20
+#define lim 100 //максимальный размер строки 
+#define lim2 20 //максимальный размер слова
+
+
 
 char * PalabraMasGrande(char * CadenaDeSimbolos){
 
-int i = 0;
-char palabra_temp[lim2];
-int contador_de_letras = 0;
-char frase[lim];
-int inicio_de_palabra_mas_grande = 0;
-int ubicacion_de_espacio[lim];
-int inicio_de_palabra = 0;
-int final_de_palabra = 0;
-int palabra_mas_grande = 0;
-int j = 0;
 
+//переменные
+int i = 0;
+int inicio_de_palabra_mas_grande = 0;
+int inicio_de_palabra = 0;
+int palabra_mas_grande = 0;
+
+
+// переменные типа "char"
+char frase[lim];
 char LaPalabraMasGrande[lim2];
 char * LaPalabra;
 
 
-strcpy(frase,CadenaDeSimbolos);
+strcpy(frase,CadenaDeSimbolos); // копирование строки "cadenadesimbolos" в строке "frase"
 
+
+//начало поиска наибольшого слова
 for(i=0;i<lim;i++){
 
-    if(frase[i] == '\0'){
-
-            if( i - inicio_de_palabra >= palabra_mas_grande ){
-
-            palabra_mas_grande = i - inicio_de_palabra;
-
-            inicio_de_palabra_mas_grande = i - palabra_mas_grande;
-
-            
-
-            i = lim;
-
-        }
-
-        }
+   
 
 
-    if(frase[i] == ' ' || frase[i] == '\t' || frase[i] == '\v'){   
+    if(frase[i] == ' ' || frase[i] == '\t' || frase[i] == '\0'){   //поиск наибольшого слова
 
         
     
@@ -57,7 +47,12 @@ for(i=0;i<lim;i++){
 
                 }
 
-                inicio_de_palabra = i;
+                if(frase[i] == '\0'){
+
+                    i = lim;
+                }
+
+                inicio_de_palabra = i+1;
 
         
 
@@ -66,23 +61,24 @@ for(i=0;i<lim;i++){
 }
 
 
-strcpy(LaPalabraMasGrande, frase + inicio_de_palabra_mas_grande);
+
+strcpy(LaPalabraMasGrande, frase + inicio_de_palabra_mas_grande); // копирование строки "frase" из позиции "inicodelapalabramasgrande" в "lapalabramasgrande" 
 
 
-for(i=palabra_mas_grande;i<lim2;i++){
+for(i=palabra_mas_grande;i<lim2;i++){ //удаление букв, которые не используются
 
     LaPalabraMasGrande[i] = '\0';
 
 }
 
 
-LaPalabra = LaPalabraMasGrande;
+LaPalabra = LaPalabraMasGrande; //копирование
 
 
 
 
 
-return LaPalabra;
+return LaPalabra; 
 
 
 
@@ -111,7 +107,7 @@ cadena = cadenaGuardado;
 
 
 
-printf("%s\n",PalabraMasGrande(cadena));
+printf("наибольщое слово:%s\n",PalabraMasGrande(cadena));
 
 
 
