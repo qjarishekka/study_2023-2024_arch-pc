c58a25a9664793fe417a132729dc373699e8f481
qjarishekka <1032234412@pfur.ru>
2024-03-07 10:19:57 +0300

    act tarea

---
+++ clases/trabajos/clase1/tarea/tarea1.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+struct nodo{
+
+        string letras;
+        int numeros;
+        double grandesnumeros;
+        nodo *next;
+
+};
+
+void addfirst( nodo *&lista, string l, int n, double gn){
+
+
+
+nodo *tmp = new nodo;
+
+while (lista != NULL){
+
+    tmp = lista->next;
+
+}
+
+tmp -> letras = letras;
+tmp -> numeros = n;
+tmp -> grandesnumeros = gn;
+
+tmp -> next = lista;
+
+lista = tmp;
+
+
+
+
+}
+
+
+void addlast(nodo * lista, string l, int n, double gn){
+
+
+
+
+}
+
+
+
+
+
+int main(){
+
+
+
+
+
+}
