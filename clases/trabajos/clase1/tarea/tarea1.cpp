faa4e40c338749175d5a1df1a54aa8f05cf62057
qjarishekka <1032234412@pfur.ru>
2024-03-07 16:05:33 +0300

    act tarea

--- clases/trabajos/clase1/tarea/tarea1.cpp
+++ clases/trabajos/clase1/tarea/tarea1.cpp
@@ -31,8 +31,72 @@ void addlast(nodo * lista, string l, int n, double gn){
 
 nodo * tmp = new nodo
 
-tmp = 
+tmp -> letras = l;
+tmp -> numeros = n;
+tmp -> grandesnumeros = gn;
 
+tmp -> next = lista;
+
+
+}
+
+int addafter(nodo*lista, string l,  int n, double gn, string nafter){
+
+    if(lista == NULL){
+
+        return 1;
+    
+    }
+
+    nodo *tmp = lista;
+
+    while(tmp!= NULL && tmp -> letras != nafter){
+         tmp = tmp -> next;
+    }
+
+    if(tmp == NULL ){
+        return 1;
+    }
+
+    nodo *newnodo = new nodo;
+
+    newnodo -> letras = l;
+    newnodo -> numeros = ca;
+    newnodo -> grandesnumeros = gn;
+    newnodo -> next = tmp -> next;
+    tmp -> next = newnodo;
+    return 0;
+
+
+
+}
+
+int addbefore(nodo *lista, string l, int n, double gn, string nbefore){
+
+
+nodo * tmp = lista;
+nodo * tmp2 = lista;
+
+while(tmp != NULL && tmp->letras != nbefore){
+
+tmp = tmp->next;
+
+if(tmp -> letras != nbefore){
+
+tmp2 = tmp;
+
+}
+
+}
+
+nodo *newnodo = new nodo;
+
+newnodo -> letras = l;
+newnodo -> numeros = n;
+newnodo -> grandesnumeros = gn;
+newnodo -> next = tmp-> next;
+
+tmp2 ->next = newnodo;
 
 }
 
---
+++ experimentos/experimentoasdf.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int n = 4;
+
+
+    do{
+
+        cout<<"teoria es verdadera";
+        n = 5;
+
+    }while(n ==4);
+
+
+
+
+while(n != 4){
+
+    cout<<"confirmacion de la teoria";
+    n = 4;
+
+}
+
+
+}
