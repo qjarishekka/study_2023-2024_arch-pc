811cf0ab65a764871bc14b9e64892678431261cd
qjarishekka <1032234412@pfur.ru>
2024-03-27 16:42:48 +0300

    tarea de programacion

--- clases/practicasdeprogramacion/tarea3/algoritmopitgot.cpp
+++ clases/practicasdeprogramacion/tarea3/algoritmopitgot.cpp
@@ -4,6 +4,32 @@ using namespace std;
 
 double calcularpi(int precision){
 
+//reservar memoria
+
+int Largo_memoria = (precision * 10) / 3;
+
+cout << Largo_memoria<< endl;
+
+int i;
+int *calculo = new int[Largo_memoria];
+
+int suma = 0;
+int presto = 0;
+
+
+for(i = 0; i < Largo_memoria; i++){
+
+    calculo[i] = 2;
+
+}
+
+
+// empieza el algoritmo
+for(i = Largo_memoria-1; i == -1; i--){  //fijarse aqui si hay error
+
+
+
+
 
 
 
@@ -13,9 +39,37 @@ double calcularpi(int precision){
 
 
 
+
+
+
+
+
+for(i = 0; i < Largo_memoria; i++){
+
+    cout<<calculo[i];
+
+}
+
+
+
+
+cout <<endl;
+return 0;
+
+}
+
+
+
+
+
 int main(){
 
+int precision;
 
+
+cin>> precision;
+
+calcularpi(precision);
 
 
 }
