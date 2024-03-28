c18edc146bca4551649a487de4cc6782612b5642
qjarishekka <1032234412@pfur.ru>
2024-03-28 16:09:42 +0300

    yoquese

---
+++ clases/trabajos/clase8/clase.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+using namespace std;
+
+double x[] = {1,2,3};
+//vect v1,v2,v3(4,x);
+//v1=v2=v3;
+
---
+++ experimentos/experimento.cpp
@@ -0,0 +1,14 @@
+// crea un tablero de ajedres
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+int e = 32 % 10;
+
+cout << e << endl;
+
+}
