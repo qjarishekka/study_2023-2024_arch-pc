f3fd63353045dfbcf167f7bc6eacfb65c2ebc7d5
qjari shekka <1032234412@pfur.ru>
2024-04-29 01:12:43 +0300

    no se que hice

--- clases/trabajos/clase11/SFML-PROJECT/coding/main.cpp
+++ clases/trabajos/clase11/SFML-PROJECT/coding/main.cpp
@@ -2,9 +2,9 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML TUTORIAL");
-    sf::CircleShape shape(200.f);
-    shape.setFillColor(sf::Color::Blue);
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
     while (window.isOpen())
     {
