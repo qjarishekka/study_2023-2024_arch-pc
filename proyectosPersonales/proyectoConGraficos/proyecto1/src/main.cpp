41fba3330e811dd17f1b69706cc429bba01f59a0
qjari shekka <1032234412@pfur.ru>
2024-09-16 20:31:55 +0300

    yo que se

--- proyectosPersonales/proyectoConGraficos/proyecto1/src/main.cpp
+++ proyectosPersonales/proyectoConGraficos/proyecto1/src/main.cpp
@@ -2,50 +2,77 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-using namespace std; 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);  
+void processInput(GLFWwindow *window);
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}
 
-int main()
-{
-    if (!glfwInit())
-    {
-        cout << "Failed to initialize GLFW" << endl;
-        return -1;
-    }
+float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+};  
 
-    glfwWindowHint(GLFW_SAMPLES, 4);
+
+
+int main(){
+
+    glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    GLFWwindow* window;
-    window = glfwCreateWindow(800, 600, "ZMMR", NULL, NULL);
-    if (window == NULL)
-    {
-        cout << "Failed to open GLFW window" << endl;
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
+GLFWwindow* window = glfwCreateWindow(800, 600, "mi primera ventana", NULL, NULL);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        cout << "Failed to initialize GLAD" << endl;
-        return -1;
-    }
-
-    glViewport(0, 0, 800, 600);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    while(!glfwWindowShouldClose(window))
-    {
-        glfwSwapBuffers(window);
-        glfwPollEvents();    
-    }
-
+if (window == NULL){
+    std::cout << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
-    return 0;
+    return -1;
+}
+
+glfwMakeContextCurrent(window);
+
+if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+}   
+
+glViewport(0, 0, 800, 600);
+
+glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+
+unsigned int VBO;
+glGenBuffers(1, &VBO);
+
+glBindBuffer(GL_ARRAY_BUFFER, VBO);  
+
+glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+while(!glfwWindowShouldClose(window)){
+
+    processInput(window);
+    glfwSwapBuffers(window);
+    glfwPollEvents();    
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+}
+
+
+glfwTerminate();
+return 0;
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+    glViewport(0, 0, width, height);
+}  
+
+void processInput(GLFWwindow *window){
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
