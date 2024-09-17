1561eb3933292b81a9c51c7e3a06e33b306606f6
qjarishekka <1032234412@pfur.ru>
2024-09-17 13:34:49 +0300

    tarea

--- segundoAño/programacionLogica/clase2/main.cpp
+++ segundoAño/programacionLogica/clase2/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "value.h"
+#include <string>
 
 using namespace std;
 
@@ -23,4 +24,24 @@ cout<< v1->sum()<<endl;
 
 
 return 0;
+}
+
+Value * parse(string str){
+
+if( str[0] == '(' && str[str.length()-1] == ')'){
+
+return parse( str.substr(1,str.length()-2));
+
+}
+
+if( str.find(',') == -1        ){
+
+    return new Atom(stof(str));
+
+}
+
+int coma = str.find(',');
+
+return Pair(parse(str.substr(0,coma)),parse(str.substr(coma)));
+
 }
