f9ecab3667b92beeb31557a103a24da5a11ab1c3
qjarishekka <1032234412@pfur.ru>
2024-09-29 01:31:38 +0300

    python

---
+++ segundoAño/python/clase3/main.py
@@ -0,0 +1,21 @@
+from Field import * 
+from Unit import *
+
+print('hello')
+ 
+field = Field( (10,10))
+
+field.out()
+
+print('///' * field.size[0])
+
+player = Jumper(field)
+player.out()
+
+command = input()
+
+while(command != 'exit'):
+    player.move(command)
+    player.out()
+    print('//'*field.size[0])
+    command = input()
