e81bfda0ab7b0ab5abe3c387d16adc730257f692
qjari shekka <1032234412@pfur.ru>
2024-11-11 20:37:03 +0300

    xd

---
+++ 2do/programacionLogica/tarea2/experimento/exp.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<string>
+#include<typeinfo>
+
+using namespace std;
+
+int permutacion(int alfa){
+
+int beta;
+
+beta = 1;
+
+while(alfa > 1){
+
+beta = beta*alfa;
+
+alfa = alfa-1;
+
+}
+
+return beta;
+}
+
+
+int combinacion(int gama, int delta ){
+
+int a = permutacion(delta);
+int b = permutacion(gama);
+int diff = delta - gama;
+int c = permutacion(diff);
+
+b = b * c;
+a = a/b;
+
+return a;
+
+}
+
+int arreglo(int epsilon, int zeta ){
+
+int d = permutacion(zeta);
+int diferencia = zeta - epsilon;
+int e = permutacion(diferencia);
+
+d = d/e;
+
+return d;
+
+}
+
+int main(){
+
+int teta = arreglo(3,5);
+
+cout<<teta;
+
+}
+
---
+++ 2do/programacionLogica/tarea2/experimento/exp2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<stdlib.h>
+using namespace std;
+
+int main(){
+
+int alfa = 1/0;
+
+cout<<alfa<<endl;
+
+
+}
