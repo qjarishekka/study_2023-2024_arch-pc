bd35aa8eb6aca3be73903bb946d0a5be48ba411f
qjari shekka <1032234412@pfur.ru>
2024-12-03 17:45:35 +0300

    tarea avanzada

--- 2do/python/tarea3/task1.py
+++ 2do/python/tarea3/task1.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import Birch
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
+from sklearn.cluster import AffinityPropagation
 
-data  = pd.read_csv('/Users/user/work/study/2023-2024/Архитектура_компьютера/tareas/mirepositorio/2do/python/tarea3/BankChurners.csv')[:1000]
-#data  = pd.read_csv('C:/work/mirepositorio/2do/python/tarea3/BankChurners.csv')[:2000]
+#data  = pd.read_csv('/Users/user/work/study/2023-2024/Архитектура_компьютера/tareas/mirepositorio/2do/python/tarea3/BankChurners.csv')[:1000]
+data  = pd.read_csv('C:/work/mirepositorio/2do/python/tarea3/BankChurners.csv')[:2000]
 
 
 #print(data.head())
@@ -28,16 +29,28 @@ data = pd.DataFrame((X_train), columns=["Customer_Age" ,"Total_Trans_Amt" ])
 #print(escalator.transform(X_train))
 
 
-brc = Birch(n_clusters=3)
-brc.fit(data)
+clusterint1 = Birch(n_clusters=3)
+clusterint1.fit(data)
 print(data)
-print(brc.labels_)
+print(clusterint1.labels_)
+
+
+clusterint2 = AffinityPropagation(random_state=None).fit(data)
 
 
 
 
-plt.scatter(data['Customer_Age'] , data['Total_Trans_Amt'], c=brc.labels_)
+
+plt.scatter(data['Customer_Age'] , data['Total_Trans_Amt'], c=clusterint1.labels_)
 plt.xlabel('Customer Age')
 plt.ylabel('Total Transactions Amount')
 plt.show()
+
+plt.scatter(data['Customer_Age'] , data['Total_Trans_Amt'], c=clusterint2.labels_)
+plt.xlabel('Customer Age')
+plt.ylabel('Total Transactions Amount')
+plt.show()
+
+
+
 
