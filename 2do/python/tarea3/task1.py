ebe01a10111e5fb3ff343a24f2ebb362c13346a7
qjarishekka <1032234412@pfur.ru>
2024-12-03 16:11:11 +0300

    tarea avanzada

--- 2do/python/tarea3/task1.py
+++ 2do/python/tarea3/task1.py
@@ -5,20 +5,15 @@ from sklearn.cluster import Birch
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 
+data  = pd.read_csv('/Users/user/work/study/2023-2024/Архитектура_компьютера/tareas/mirepositorio/2do/python/tarea3/BankChurners.csv')[:1000]
+#data  = pd.read_csv('C:/work/mirepositorio/2do/python/tarea3/BankChurners.csv')[:2000]
 
-data  = pd.read_csv('C:/work/mirepositorio/2do/python/tarea3/BankChurners.csv')[:2000]
-
-plt.scatter(data['Customer_Age'], data['Total_Trans_Amt'])
 
 #print(data.head())
 ##print(data.tail())
 #print(data.dtypes)
 
 X = data.iloc[:,[2,17]]
-
-
-#print(X.head())
-#y.head()
 
 
 X_train, X_test = train_test_split(X, random_state=50)
@@ -28,19 +23,20 @@ X_train, X_test = train_test_split(X, random_state=50)
 
 escalator = MinMaxScaler().fit(X_train)
 
-data = pd.DataFrame(escalator.transform(X_train), columns=["Customer_Age" ,"Total_Trans_Amt" ])
+data = pd.DataFrame((X_train), columns=["Customer_Age" ,"Total_Trans_Amt" ])
 
 #print(escalator.transform(X_train))
 
 
+brc = Birch(n_clusters=3)
+brc.fit(data)
+print(data)
+print(brc.labels_)
 
 
-""" data = pd.read_csv(escalator.transform(y)) """
 
 
-
-
-
+plt.scatter(data['Customer_Age'] , data['Total_Trans_Amt'], c=brc.labels_)
 plt.xlabel('Customer Age')
 plt.ylabel('Total Transactions Amount')
 plt.show()
