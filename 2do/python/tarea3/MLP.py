b18d0fa9112ca9905b039ffb58cd81cf7d2f6661
qjarishekka <1032234412@pfur.ru>
2024-12-18 13:53:35 +0300

    xd

--- 2do/python/tarea3/MLP.py
+++ 2do/python/tarea3/MLP.py
@@ -5,24 +5,26 @@ from sklearn.model_selection import train_test_split
 from sklearn import neural_network
 from sklearn import preprocessing
 
-#data  = pd.read_csv('/Users/user/work/study/2023-2024/Архитектура_компьютера/tareas/mirepositorio/2do/python/tarea3/BankChurners.csv')[:1000]
-data  = pd.read_csv('C:/work/mirepositorio/2do/python/tarea3/BankChurners.csv')[:2000]
+data  = pd.read_csv('/Users/user/work/study/2023-2024/Архитектура_компьютера/tareas/mirepositorio/2do/python/tarea3/BankChurners.csv')[:1000]
+#data  = pd.read_csv('C:/work/mirepositorio/2do/python/tarea3/BankChurners.csv')[:2000]
 
-#data.dropna(inplace=True)
+data.dropna(inplace=True)
 
 X = data.iloc[:,[9,10,11,12,17]]
-Y = data.iloc[:,2]
+data['Marital_Status'] = data['Marital_Status'].replace({'Single' : 1 ,'Married' : 2 , 'Divorced' : 3 , 'Unknown' : 4 })
+Y = data.iloc[:,6]
+
 
 
 x_train, x_test, y_train, y_test = train_test_split(X, Y ,test_size= 0.5)
 
-data_min_max = preprocessing.StandardScaler().fit_transform(x_train)
+#data_min_max = preprocessing.RobustScaler().fit_transform(x_train)
 
 
 
 
-classification1 = neural_network.MLPClassifier(max_iter=5000) 
-classification1.fit(data_min_max , y_train)
+classification1 = neural_network.MLPClassifier(random_state=4,max_iter=5000) 
+classification1.fit(x_train, y_train)
 y_pred = classification1.predict(x_test)
 
 
