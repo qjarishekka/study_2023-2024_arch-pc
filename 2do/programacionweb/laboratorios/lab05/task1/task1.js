24732902dca9d783626c5edaddd2ec14dd43c7e7
qjari shekka <1032234412@pfur.ru>
2024-12-22 14:25:16 +0300

    task2

--- 2do/programacionweb/laboratorios/lab05/task1/task1.js
+++ 2do/programacionweb/laboratorios/lab05/task1/task1.js
@@ -1,9 +1,40 @@
 let array = [];
-array.push("X");
-document.write(array)
+//array.push("X");
+//document.write(array)
+
 function anotherX(){
-    array.puxh("X");
-    document.write(array);
-    alert("el codigo se ejecuto")
+    array.push("X");
+    //document.write(array);
+    //alert("el codigo se ejecuto")
+    document.getElementById("first").innerHTML=array;
 }
 
+
+function extratequivalent(){
+
+let val = document.getElementsByTagName("td")
+
+let days = document.getElementById("days")
+
+let selecValue = days.value
+
+
+
+for(let i = 0 ; i < 7 ; i++){
+
+    let text = val[i*2].textContent
+
+
+    if( text.includes(selecValue) ){
+        document.getElementById('answer').innerHTML = val[i*2 + 1].innerHTML
+    }
+
+
+}
+
+//console.log("se ejecuta" ,selecValue, val)
+
+//document.getElementById("answer").innerHTML = val[1].innerHTML
+//document.getElementById("answer").innerHTML = val
+
+}
