65b3d948e95b466505520d09caa459d613583e1d
qjari shekka <1032234412@pfur.ru>
2024-12-22 14:28:16 +0300

    xd

--- 2do/programacionweb/laboratorios/lab05/task1/task1.js
+++ 2do/programacionweb/laboratorios/lab05/task1/task1.js
@@ -10,31 +10,3 @@ function anotherX(){
 }
 
 
-function extratequivalent(){
-
-let val = document.getElementsByTagName("td")
-
-let days = document.getElementById("days")
-
-let selecValue = days.value
-
-
-
-for(let i = 0 ; i < 7 ; i++){
-
-    let text = val[i*2].textContent
-
-
-    if( text.includes(selecValue) ){
-        document.getElementById('answer').innerHTML = val[i*2 + 1].innerHTML
-    }
-
-
-}
-
-//console.log("se ejecuta" ,selecValue, val)
-
-//document.getElementById("answer").innerHTML = val[1].innerHTML
-//document.getElementById("answer").innerHTML = val
-
-}
---
+++ 2do/programacionweb/laboratorios/lab05/task1/task2.js
@@ -0,0 +1,24 @@
+function extratequivalent(){
+
+    let val = document.getElementsByTagName("td")
+    
+    let days = document.getElementById("days")
+    
+    let selecValue = days.value
+    
+    
+    
+    for(let i = 0 ; i < 7 ; i++){
+    
+        let text = val[i*2].textContent
+    
+    
+        if( text.includes(selecValue) ){
+            document.getElementById('answer').innerHTML = val[i*2 + 1].innerHTML
+        }
+    
+    
+    }
+    
+    }
+    
