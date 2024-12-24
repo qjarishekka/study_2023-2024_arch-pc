cd08f55d2ba9d3d32ed8b2f5c73ffa2385ae39c0
qjarishekka <1032234412@pfur.ru>
2024-12-24 11:33:36 +0300

    terminado

--- 2do/python/clase9/Calc/Task.py
+++ 2do/python/clase9/Calc/Task.py
@@ -1,14 +1,16 @@
 class Task:
-    def __init__(self,formula):
-        self.formula = formula
+    def __init__(self,f):
+        self.formula = f
     # 1 + 2*3 - 5/4
     def compute(self):
         operators = ['+', '-' , '*' , '/']
         for op in operators:
             if op in self.formula:
                 index = self.formula.index(op)
+                
                 left = Task(self.formula[:index])
                 right = Task(self.formula[index+1:])
+                
                 match op:
                     case '+':
                         return left.compute() + right.compute()
@@ -19,3 +21,4 @@ class Task:
                     case '*':
                         return left.compute() * right.compute()
         return float(self.formula)
+
--- 2do/python/clase9/Calc/__init__.py
+++ 2do/python/clase9/Calc/__init__.py
@@ -4,13 +4,13 @@ else:
     from Calc.Task import Task
 
 def test1(comments=False):
-    test = Task('1+1')
-    print(test)
-    #assert test == 2
+    test = Task("1+1")
+    
+    assert test.compute() == 2
     if comments:
         print('test 1.1 completed')
-    test = Task('2*3')
-    #assert test == 6
+    test = Task("2*3")
+    assert test.compute() == 6
     if comments:
         print('test 1.2. completed')
 
