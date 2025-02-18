ad75c33e059cec9d1eca504fe245e665b3e120e7
qjarishekka <qjarishekka@pfur.ru>
2025-02-18 16:12:53 +0300

    clase 2

--- 2do/EstructuraDeDatos/App.java
+++ 2do/EstructuraDeDatos/App.java
@@ -1,31 +1,48 @@
 import javax.swing.*;
 import java.awt.*;
+
 public class App extends JFrame{
 
     App(){
         super("My first app");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(480,380);
+        setSize(1280,720); //1280x720
         setLayout(null);
 
-        JLabel label = new JLabel("hello its me michael");
+        JLabel label = new JLabel("hello its me Q'jari");
         label.setBounds(20,20,220, 40);
         label.setFont(new Font("Papyrus", Font.ITALIC, 20));
         label.setBackground(Color.RED);
         label.setForeground(Color.GREEN);
         label.setOpaque(true);
 
+        JLabel pic = new JLabel(new ImageIcon("2do\\EstructuraDeDatos\\imagenRandom.jpg"));
+        pic.setBounds(20,110,160,60 );
+        add(pic);
+
 
 
         add(label);
 
 
-        JButton button = new JButton("click me");
+        JButton button = new JButton("click me"); //PRIMER BOTON
         button.setBounds(40,60,160,40);
         add(button);
 
-        MyButtonlistener listener = new MyButtonlistener();
+       /*  JButton button2 = new JButton("click me"); //SEGUNBDO BOTON
+        button2.setBounds(40,120,160,40);
+        add(button2);
+
+        MyButtonlistener listener2 = new MyButtonlistener(button); // escuchador del segundo boton
+        //listener.button = button; se puede hacer asi pero no se recomienda
+        button2.addActionListener(listener2); */
+
+        MyButtonlistener listener = new MyButtonlistener(pic,10,10); // escuchador del primer boton
         button.addActionListener(listener);
+        //button.addActionListener(listener2);
+
+
+        
         
 
 
--- 2do/EstructuraDeDatos/MyButtonlistener.java
+++ 2do/EstructuraDeDatos/MyButtonlistener.java
@@ -1,12 +1,29 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JComponent;
+
 public class MyButtonlistener implements ActionListener{
-    
+    private JComponent component;
+    private int dx, dy;
+    int counter = 0;
+
+    public MyButtonlistener(JComponent component, int dx, int dy){
+        this.component = component;
+        this.dx = dx;
+        this.dy = dy;
+    }
+
     @Override
     public void actionPerformed(ActionEvent e){
-        System.out.println("Clicked");
+        component.setLocation(component.getX() + dx , component.getY() + dy);
+        //button.setText("clicked " + counter);
+        counter++;
     }
+
+    //donde hay component habia Button, esto se hizo para poder mover las imagenes
+    // en caso de querer mover un boton se puede usar volver a cambiar, pero la clase JComponent es mas general y nos permite usar esta funcion en mas casos
 
 
 }
