02ca7e888fd61f402b1ae8e6b8af2160e98a510e
qjari shekka <1032234412@pfur.ru>
2025-03-21 14:54:14 +0300

    tarea

--- 2do/EstructuraDeDatos/JuegoTarea/Main.java
+++ 2do/EstructuraDeDatos/JuegoTarea/Main.java
@@ -1,12 +1,17 @@
 package JuegoTarea;
 import java.awt.*;
+import java.util.Observer;
+
 import javax.swing.*;
+
+import juegoclase.RotImage;
 
 
 public class Main extends JFrame {
 
-    boolean playing = false;
-
+    boolean Start = false;
+    int screenSize[] = new int [2];
+    static Main proxy;
     
     
 
@@ -15,35 +20,78 @@ public class Main extends JFrame {
         super("MyGame");
 
         Toolkit t = Toolkit.getDefaultToolkit();
-        double screenSize[] = new double [2];
-        screenSize[0] = t.getScreenSize().getWidth();
-        screenSize[1] = t.getScreenSize().getHeight();
-
-
         
+        screenSize[0] = (int)t.getScreenSize().getWidth();
+        screenSize[1] = (int)t.getScreenSize().getHeight();        
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize((int)screenSize[0],(int)screenSize[1]);
         setLayout(null);
         //setUndecorated(true);
+
+        
 
         setVisible(true);
 
     }
 
     public void sceneManager(){
+
+        if(!Start){
+            scene1();
+        }else{
+            scene2();
+        }
         
+        
+
+
+    }
+
+    public void scene1(){
+
+        
+
+        Image backGroundImage  = new ImageIcon("src//backGround.png").getImage();
+        JLabel backGround = new JLabel(new ImageIcon(backGroundImage.getScaledInstance((int)screenSize[0], (int)screenSize[1],0 )));
+        
+
+
+        backGround.setBounds(0 ,0 , (int)screenSize[0], (int)screenSize[1]);
+        
+        JButton playButton = new JButton();
+        int playButtonWidth = screenSize[0] * 30 / 100;
+        int playButttonHeight = screenSize[1] * 20 / 100;
+        playButton.setBounds( (screenSize[0]/2) - (playButtonWidth/2)  ,(screenSize[1]/2) - (playButttonHeight/2),playButtonWidth,playButttonHeight);
+        Image playImage = new ImageIcon("src//play.png").getImage();
+        JLabel play = new JLabel(new ImageIcon(playImage.getScaledInstance(playButtonWidth, playButttonHeight, 0)));
+        
+        
+        
+        add(playButton);
+        playButton.setIcon(play.getIcon());;
+        add(backGround);
+        
+        repaint();
+        
+        
+        
+        
+
+    }
+
+    public void scene2(){
+
     }
 
     
 
 
-    static Main proxy;
+    
     public static void main(String[] args){
         
-        proxy = new Main();        
-        
-        
-
+        proxy = new Main();   
+        proxy.sceneManager();   
+          
        
     }
 
