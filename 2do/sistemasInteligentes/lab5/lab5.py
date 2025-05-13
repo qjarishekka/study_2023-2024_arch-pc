b50be7b157c8366f0452f0fbcb542b5862a8915c
qjari <qjarishek@outlook.com>
2025-05-13 14:38:51 +0300

    xzd

--- 2do/sistemasInteligentes/lab5/lab5.py
+++ 2do/sistemasInteligentes/lab5/lab5.py
@@ -15,13 +15,14 @@ def calculate_model(w , b , x):
 
 def descending_gradient(w_ , b_ , alpha, x ,y):
     N = x.shape[0]
+    #print(N)
     dw = -(2/N)*np.sum(x*(y-(w_* x  + b_)))
     db = -(2/N)*np.sum(y-(w_* x  + b_))
     w = w_ - (alpha*dw)
     b = b_ - (alpha*db)
     return w, b
 
-dataset = pd.read_csv('insurance.csv')[:50]
+dataset = pd.read_csv('insurance.csv')[:10]
 dataset.plot(x = 'age', y = 'charges'  , style = 'o')
 
 #dataset['sex'] = dataset['sex'].replace({'male': 0 , 'female': 1})
@@ -30,56 +31,55 @@ dataset.plot(x = 'age', y = 'charges'  , style = 'o')
 X = dataset[['age']]
 y = dataset[['charges']]
 
-#print(X)
-
+print(X)
+print(y)
 
 x_train, x_test, y_train, y_test = train_test_split(X,y,test_size= 0.25, random_state=0)
 
-#x_train , y_train = MinMaxScaler().fit_transform(x_train, y_train)
+x_train = x_train.values.reshape([x_train.values.shape[0],1])
+x_test = x_test.values.reshape([x_test.values.shape[0],1])
+y_train = y_train.values.reshape([y_train.values.shape[0],1])
+y_test = y_test.values.reshape([y_test.values.shape[0],1])
 
-#x_train_scaled = MinMaxScaler().fit_transform(x_train)
-#y_train_scaled = MinMaxScaler().fit_transform(y_train)
 
 
 poly  = PolynomialFeatures(degree=3)
 
 x_train = poly.fit_transform(x_train)
-y_train = poly.fit_transform(y_train)
+
 print(x_train)
+print("---------------")
+poly.fit(x_train , y_train)
+#y_train = poly.fit_transform(y_train)
+print(x_train)
+print("---------------")
+print(y_train)
 
-#x_train_scaled_polynomial = PolynomialFeatures(degree= 3, interaction_only=False).fit_transform(x_train)
-#y_train_scaled_polynomial = PolynomialFeatures(degree= 3,interaction_only=False).fit_transform(y_train)
+#print(y_train)
 
-
-#print(x_train)
-#print(y_train_scaled_polynomial)
-
+x_train = MinMaxScaler().fit_transform(x_train)
+print(x_train)
 
 """ def calculate_error(y , y_):
     N = y.shape[0]
     error =np.sum((y-y_)**2)/N
     return error """
-#print(x_train_scaled.size)
-#print(x_train_scaled_polynomial.size)
-
-
 
 np.random.seed(2)
 w = np.random.randn(1)[0]
 b = np.random.randn(1)[0]
 
+
 alpha = 0.0004
 nits = 40000
-
 for i in range(nits):
     [w,b] = descending_gradient(w,b,alpha , x_train , y_train)
     #[w,b] = descending_gradient(w,b,alpha , x_train_scaled , y_train_scaled)
     y_ = calculate_model(w,b,x_train)
 
  
-
-
 #x_predict = np.linspace(0,5 ,100)
+print(x_test , w , b)
 y_regr = calculate_model(w,b,x_test)
 #print(y_regr)
 plt.plot(x_test,y_regr,'r')
